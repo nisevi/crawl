e5ca93b5d946b498bd351217d5388dbdf93220ef
nisevi <nicolas.s.vidal@gmail.com>
2018-05-03 16:03:40 +0200

    New version minor fixes.

--- spec/lib/just_crawl/version_spec.rb
+++ spec/lib/just_crawl/version_spec.rb
@@ -3,6 +3,6 @@ require_relative '../../../lib/just_crawl/version'
 
 RSpec.describe 'Gem version' do
   it 'should match the current gem version' do
-    expect(JustCrawl::VERSION).to eq '1.1.8'
+    expect(JustCrawl::VERSION).to eq '1.1.9'
   end
 end
